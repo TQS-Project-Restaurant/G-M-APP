054dbc0e3b047d20cce1f7af9f102b09293521f7
Tomas Victal <tomasvictal@gmail.com>
2024-06-02 23:08:55 +0100

    axios client

---
+++ services/client.ts
@@ -0,0 +1,38 @@
+import { useUserStore } from "@/stores/useUserStore";
+import axios from "axios";
+
+const Client = axios.create({
+    baseURL: process.env.EXPO_PUBLIC_API_URL,
+    timeout: 5000,
+  });
+
+Client.interceptors.request.use(
+  (config) => {
+    if(useUserStore.getState().logged){
+      config.headers.Authorization = `Bearer ${useUserStore.getState().token}`;
+    }
+    return config
+  },
+  (error) =>{
+    return Promise.reject(error);
+  }
+)
+
+Client.interceptors.response.use(
+    function (response){
+      return response; 
+  
+    }, 
+    function(error){
+      const logout = useUserStore.getState().logout
+  
+      if (error.response.status === 403){
+        logout()
+      }
+  
+      return Promise.reject(error);
+    }
+  )
+
+export default Client;
+  
