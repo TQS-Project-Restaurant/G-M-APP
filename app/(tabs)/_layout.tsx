335f4f83a4034daa7041a3dcc79094589ee170cb
Tomas Victal <tomasvictal@gmail.com>
2024-06-02 23:10:37 +0100

    change tabs logout button test

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,14 @@
-import { Tabs } from 'expo-router';
+import { Tabs, router } from 'expo-router';
 import React from 'react';
 
 import { TabBarIcon } from '@/components/navigation/TabBarIcon';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { useUserStore } from '@/stores/useUserStore';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
+
 
   return (
     <Tabs
@@ -28,7 +30,16 @@ export default function TabLayout() {
         options={{
           title: 'Explore',
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+            <TabBarIcon name={focused ? 'albums-outline' : 'albums-outline'} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="bookings"
+        options={{
+          title: 'Bookings',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'albums' : 'albums-outline'} color={color} />
           ),
         }}
       />
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,36 +1,20 @@
-import { View,Text,Image, StyleSheet, Platform } from 'react-native';
+import { useUserStore } from '@/stores/useUserStore';
+import { Link } from 'expo-router';
+import { View,Text,Image, StyleSheet, Platform, Button } from 'react-native';
 
 export default function HomeScreen() {
+  const Logout = useUserStore((state:any) => state.logout)
   return (
-    <View className="flex-1 justify-center items-center bg-slate-500 rounded-md">
+    <View className="flex-1 flex justify-center items-center bg-slate-500 rounded-md">
       <View className='bg-green-500 rounded-md p-2 h-[20%]'>
         <Text className="text-white">woof</Text>
         <Text className=''>pao</Text>
       </View>
       <View className="justify-center items-center bg-red-500 rounded-md">
         <Text className="text-white">woof2</Text>
-        <Text className=''>pao2</Text>
       </View>
+      <Button title='logout' onPress={Logout}></Button>
     </View>
     
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
